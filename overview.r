e517b28402a52988322023cd4be64d6c9625f8e4
mhafizhussin <mhafizhussin93@gmail.com>
2018-05-05 10:58:26 +0800

    Add files via upload

--- overview.r
+++ overview.r
@@ -8,6 +8,7 @@ library(bubbles)
 library(formattable)
 library(knitr)
 library(kableExtra)
+library(ggplot2)
 
 # global constant
 color1 <- c("#6600FF","#0066FF","#CCFF00","#00CCFF","#FF00CC","#66FF00","#00FF66","#00FF00","#FFCC00",
@@ -15,6 +16,21 @@ color1 <- c("#6600FF","#0066FF","#CCFF00","#00CCFF","#FF00CC","#66FF00","#00FF66
 color2 <- c("#CC00FF","#0000FF","#FF0000","#FF6600","#00FFCC","#FF0066",
             "#6600FF","#0066FF","#CCFF00","#00CCFF","#FF00CC","#66FF00","#00FF66","#00FF00","#FFCC00")
 university <- c("UM","USM","UKM","UPM","UTM")
+
+Gender <- c("MALE_STUDENT","FEMALE_STUDENT","MALE_STAFF","FEMALE_STAFF")
+Student_Level <- c("PHD_STUDENT", "MASTER_STUDENT", "BACHELOR_STUDENT","DIPLOMA_STUDENT")
+Nationality <- c("MALAYSIAN_STUDENT", "INTERNATIONAL_STUDENT")
+Disability <- c("HEARING_DISABILITY","SPEECH_DISABILITY", "PHYSICAL_DISABILTY", "VISUAL_DISABILITY", "OTHERS_DISABILITY")
+Staff_Qualification <- c("PHD_HOLDER", "MASTERS_HOLDER", "BACHELOR_HOLDER")
+Staff_Position <- c("PROFESSORS","ASSOC_PROFS","LECTURERS")
+choices_var <- c("Gender", "Student_Level", "Nationality", "Disability", "Staff_Qualification", "Staff_Position")
+
+# tables
+Um_Table <- read_excel("Main_Data.xlsx", sheet = "UM")
+Usm_Table <- read_excel("Main_Data.xlsx", sheet = "USM")
+Ukm_Table <- read_excel("Main_Data.xlsx", sheet = "UKM")
+Upm_Table <- read_excel("Main_Data.xlsx", sheet = "UPM")
+Utm_Table <- read_excel("Main_Data.xlsx", sheet = "UTM")
 
 header <- dashboardHeader(title = "SAMPLE")
 
@@ -46,17 +62,24 @@ body <- dashboardBody(
       
       fluidRow(
         box(
-          title = "Data for Application", height = 800, width = 12, solidHeader = TRUE,
-          status = "primary",
-          tableOutput("table")
+          title = "University Times Series Visualization", width = 12, height = 40, solidHeader = TRUE, status = "warning"
         )
       ),
       
       fluidRow(
+        box(width = 12, background = "blue", 
+            selectInput("inputX","Select variable to display time series",choices = choices_var, multiple = FALSE,selected = "Gender" ))
+      ),
+      
+      fluidRow(
+        box(width = 12, height = 400, background = "navy", plotOutput("plot2", height = 380 ))
+      ),
+      
+      fluidRow(
         box(
-          title = "Graph of Enrolment, Intake and Output for 2017", height = 500,width = 12, solidHeader = TRUE,
+          title = "Data for Application", height = 800, width = 12, solidHeader = TRUE,
           status = "primary",
-          plotOutput("plot2", height = 400)
+          tableOutput("table")
         )
       )
     ),
@@ -77,9 +100,6 @@ body <- dashboardBody(
           tabPanel("Student by States", "Mouseover to focus on each chord to see the information.
                     The thickness of links between chords encodes the number of students:thicker links represent 
                     more students.")
-          # column(width = 12, div(class="col-md-6", h3("Mouseover to focus on each chord to see the information.\n
-                                                      # The thickness of links between chords encodes the number of students:thicker links represent 
-                                                      # more students.")))
         ),
         chorddiagOutput("distPlot", height = 600)
       )
@@ -91,9 +111,6 @@ body <- dashboardBody(
       ),
       
       fluidPage(
-        br(),
-        br(),
-        
         selectInput('input_data2', "Choose one variable to view the corresponding Chord Diagram",
                     choices = c("Gender","Education Qualification", "Academic Position"),
                     selected = 'Gender',
@@ -136,18 +153,66 @@ shinyApp(
   ui = dashboardPage(header, sidebar, body),
   server = function(input, output) {
     output$plot2 <- renderPlot({
-      barplot(c(7,5,8,20,3),
-              names.arg=c("Category 1","Category 2","Category 3","Category 4","Category 5"),
-              ylim=c(0,25),
-              col=c("beige","orange","lightgreen","lightblue","yellow"),
-              ylab="Count of items")
+      print(paste0("Current Uni is: ", input$input_table))
+      print(paste0("Current output is: ", input$inputX))
+      
+      
+      if(input$input_table == "UM"){
+        u_data <- read_excel("Main_Data.xlsx", sheet = "UM")
+      } else if (input$input_table == "USM") {
+        u_data <- read_excel("Main_Data.xlsx", sheet = "USM")
+      } else if (input$input_table == "UKM") {
+        u_data <- read_excel("Main_Data.xlsx", sheet = "UKM")
+      } else if (input$input_table == "UPM") {
+        u_data <- read_excel("Main_Data.xlsx", sheet = "UPM")
+      } else {
+        u_data <- read_excel("Main_Data.xlsx", sheet = "UTM")
+      }
+      
+      transpose_data <- transpose(u_data)
+      colnames(transpose_data) <- u_data$VARIABLES
+      transpose_data <- transpose_data[-1,]
+      
+      # "Gender", "Student_Level", "Nationality", "Disability", "Staff_Qualification", "Staff_Position"
+      
+      if (input$inputX == "Gender") {
+        input_len = length(Gender)
+        input_str = Gender
+      } else if(input$inputX == "Nationality") {
+        input_len = length(Nationality)
+        input_str = Nationality
+      } else if(input$inputX == "Student_Level") {
+        input_len = length(Student_Level)
+        input_str = Student_Level
+      } else if(input$inputX == "Disability") {
+        input_len = length(Disability)
+        input_str = Disability
+      } else if(input$inputX == "Staff_Qualification") {
+        input_len = length(Staff_Qualification)
+        input_str = Staff_Qualification
+      } else {
+        input_len = length(Staff_Position)
+        input_str = Staff_Position
+      }
+      
+      year <- rep(as.numeric(c("2015","2016","2017")),times = length(input_len))
+      newData <- transpose_data %>% select(input_str) %>% gather(key = variable, value = value)
+      newData$year <- year
+      newData$value <- as.numeric(newData$value)
+      
+      # Multiple line plot
+      ggplot(newData, aes(x = year, y = value)) + 
+        geom_line(aes(color = variable), size = 1) +
+        scale_color_manual(values = rainbow(input_len, alpha = NULL)[sample(input_len)]) +
+        theme_minimal()
+      
     })
     
     output$table <- renderTable({
       # read from Main_data
       
       if (input$input_table == "UM") {
-        Um_Table <- read_excel("Main_Data.xlsx", sheet = "UM")
+        # Um_Table <- read_excel("Main_Data.xlsx", sheet = "UM")
         Um_Table
         # Um_Table %>% 
           # select(VARIABLES, `2015`,`2016`,`2017`) %>%
@@ -156,16 +221,12 @@ shinyApp(
           # kable_styling(bootstrap_options = c("striped", "condensed", "bordered"), full_width = TRUE)
         
       } else if (input$input_table == "USM") {
-        Usm_Table <- read_excel("Main_Data.xlsx", sheet = "USM")
         Usm_Table
       } else if (input$input_table == "UKM") {
-        Ukm_Table <- read_excel("Main_Data.xlsx", sheet = "UKM")
         Ukm_Table
       } else if (input$input_table == "UPM") {
-        Upm_Table <- read_excel("Main_Data.xlsx", sheet = "UPM")
         Upm_Table
       } else {
-        Utm_Table <- read_excel("Main_Data.xlsx", sheet = "UTM")
         Utm_Table
       }
     })
@@ -176,20 +237,17 @@ shinyApp(
       main_data <- read_excel("Main_Data.xlsx")
       
       # local constant
-      gender_variables <-c("MALE_2015","MALE_2016","MALE_2017",
-                           "FEMALE_2015","FEMALE_2016","FEMALE_2017")
+      gender_variables <-c("MALE_2015","MALE_2016","MALE_2017", "FEMALE_2015","FEMALE_2016","FEMALE_2017")
+      
       citizen_variables <- c("MALAYSIAN_2015","MALAYSIAN_2016","MALAYSIAN_2017",
                              "INTERNATIONAL_2015","INTERNATIONAL_2016","INTERNATIONAL_2017")
-      level_variables <- c("DIPLOMA_2015","DIPLOMA_2016","DIPLOMA_2017",
-                           "BACHELOR_2015","BACHELOR_2016","BACHELOR_2017",
-                           "MASTER_2015","MASTER_2016","MASTER_2017",
-                           "PHD_2015","PHD_2016","PHD_2017")
-      disable_variables <- c("HEARING_15","HEARING_16","HEARING_17",
-                             "SPEECH_15","SPEECH_16","SPEECH_17",
-                             "PHYSICAL_15","PHYSICAL_16","PHYSICAL_17",
-                             "SPEECH_15","SPEECH_16","SPEECH_17",
-                             "VISUAL_15","VISUAL_16","VISUAL_17",
-                             "OTHERS_15","OTHERS_16","OTHERS_17")
+      
+      level_variables <- c("DIPLOMA_2015","DIPLOMA_2016","DIPLOMA_2017", "BACHELOR_2015","BACHELOR_2016","BACHELOR_2017",
+                           "MASTER_2015","MASTER_2016","MASTER_2017", "PHD_2015","PHD_2016","PHD_2017")
+      
+      disable_variables <- c("HEARING_15","HEARING_16","HEARING_17","SPEECH_15","SPEECH_16","SPEECH_17",
+                             "PHYSICAL_15","PHYSICAL_16","PHYSICAL_17","SPEECH_15","SPEECH_16","SPEECH_17",
+                             "VISUAL_15","VISUAL_16","VISUAL_17","OTHERS_15","OTHERS_16","OTHERS_17")
       
       
       # select data
@@ -229,9 +287,11 @@ shinyApp(
       # local constant
       gender_var <- c("MALE_STAFF_2015", "MALE_STAFF_2016", "MALE_STAFF_2017",
                       "FEMALE_STAFF_2015","FEMALE_STAFF_2016","FEMALE_STAFF_2017")
+      
       level_var <- c("PHD_STAFF_2015", "PHD_STAFF_2016", "PHD_STAFF_2017",
                      "MASTERS_STAFF_2015", "MASTERS_STAFF_2016", "MASTERS_STAFF_2017",
                      "BACHELOR_STAFF_2015", "BACHELOR_STAFF_2016", "BACHELOR_STAFF_2017")
+      
       position_var <- c("PROFESSORS_2015", "PROFESSORS_2016", "PROFESSORS_2017",
                         "ASSOC_PROFS_2015", "ASSOC_PROFS_2016", "ASSOC_PROFS_2017",
                         "LECTURERS_2015", "LECTURERS_2016", "LECTURERS_2017")
